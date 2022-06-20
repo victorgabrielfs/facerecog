ffa227cc58b9ea0aef0118beeccec6b904f36050
Victor Gabriel <victor.g.filgueiras@gmail.com>
2022-06-20 16:06:18 -0300

    Finished interface and CRUD features

--- app/controller/routes.py
+++ app/controller/routes.py
@@ -1,8 +1,9 @@
+import mimetypes
 from app import app, db
 from flask import render_template, redirect, url_for, flash, Response, request, send_file
 
 from app.controller.recog.recog_utils import gen
-from app.model.forms import LoginForm, SignUpForm
+from app.model.forms import EditUserPassword, LoginForm, SignUpForm, MissingPersonForm, EditUserForm, EditMissingPerson
 from app.model.tables import User, bcrypt, Images, MissingPeople
 from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.utils import secure_filename
@@ -51,9 +52,9 @@ def signup():
     form = SignUpForm()
     if form.validate_on_submit():
 
-        print(form.photo.data)
+
         users_like_yours = User.query.filter_by(email=form.email.data).first()
-        print(users_like_yours)
+
         if users_like_yours is not None:
             flash("There is already an user with this email")
             return redirect(url_for('signup'))
@@ -168,4 +169,152 @@ def get_missing_person_images(id):
 @login_required
 def get_missing_person(id):
     missing_person = MissingPeople.query.filter_by(id=id).first()
-    return render_template("missing_person.html", missing_person=missing_person)
+    form = EditMissingPerson(
+        name=missing_person.name, 
+        birthday=missing_person.birthday,
+        birthplace=missing_person.birthplace,
+        place_of_disappearance=missing_person.place_of_disappearance,
+        disappearance_details=missing_person.disappearance_details
+    )
+   
+    return render_template("missing_person.html", missing_person=missing_person, form=form, id=id)
+
+@app.route('/update_missing_person/<int:id>', methods=['POST'])
+@login_required
+def update_missing_person(id):
+    form = EditMissingPerson()
+    missing_person = MissingPeople.query.filter_by(id=id).first()
+    if form.validate_on_submit():
+            if form.photo.data == None:
+                missing_person.name = form.name.data
+                missing_person.birthday = form.birthday.data
+                missing_person.birthplace = form.birthplace.data
+                missing_person.place_of_disappearance = form.place_of_disappearance.data
+                missing_person.disappearance_details = form.disappearance_details.data
+                db.session.commit()
+            else:
+                pics_list = missing_person.pics
+                
+                pic = form.photo.data
+                pics_list[0].picture = pic.read()
+                pics_list[0].filename = secure_filename(pic.filename)
+                pics_list[0].mimetype = pic.mimetype
+                missing_person.name = form.name.data
+                missing_person.birthday = form.birthday.data
+                missing_person.birthplace = form.birthplace.data
+                missing_person.place_of_disappearance = form.place_of_disappearance.data
+                missing_person.disappearance_details = form.disappearance_details.data
+                db.session.commit()
+            return redirect(url_for('missing_people'))
+    flash('form, not valid')        
+    return redirect(url_for('missing_people'))
+
+
+@app.route('/add_missing_person', methods=['GET', 'POST'])
+@login_required
+def add_missing_person():
+    form = MissingPersonForm()
+    if request.method =='GET':
+        return render_template("add_missing_person.html", form=form)
+    else:
+        if form.validate_on_submit():
+            
+            user_id = current_user.id
+            name = form.name.data
+            birthday = form.birthday.data
+            birthplace = form.birthplace.data
+            place_of_disappearance = form.place_of_disappearance.data
+            disappearance_details = form.disappearance_details.data
+            pic = form.photo.data
+            filename = secure_filename(pic.filename)
+            mimetype = pic.mimetype
+            image = Images(picture=pic.read(), isUserProfile=False, isMissingPersonProfile=True, filename=filename,
+                   mimetype=mimetype)
+
+            missing_person = MissingPeople(
+                user_id=user_id,
+                name=name,
+                birthday=birthday,
+                birthplace=birthplace, 
+                place_of_disappearance=place_of_disappearance,
+                disappearance_details=disappearance_details,
+                pics=[image])
+            
+            db.session.add(image)
+            db.session.add(missing_person)
+            db.session.commit()
+            return redirect(url_for("missing_people"))
+
+
+    
+
+
+@app.route('/edit_user', methods=['GET', 'POST'])
+@login_required
+def edit_user():
+    
+    if request.method == 'GET':
+        form = EditUserForm(
+        name=current_user.name,
+        email=current_user.email
+    )
+        return render_template('edit_user.html', form=form)
+    if request.method == 'POST':
+        form = EditUserForm()
+        user = current_user
+        if form.validate_on_submit():
+            if form.photo.data == None:
+                user.name = form.name.data
+                user.email = form.email.data
+                db.session.commit()
+            else:
+                pic_row = current_user.profile_pic
+                
+                pic = form.photo.data
+                pic_row.picture = pic.read()
+                pic_row.filename = secure_filename(pic.filename)
+                pic_row.mimetype = pic.mimetype
+                user.name = form.name.data
+                user.email = form.email.data
+                db.session.commit()
+            return redirect(url_for('home'))     
+        
+        return 'Bad request',404
+    return 'Bad request',404    
+
+@app.route('/edit_user/password', methods=['GET', 'POST'])
+@login_required
+def edit_user_password():
+    form = EditUserPassword()
+    if request.method == 'POST':
+        if current_user.verify_password(form.current_password.data):
+            password_hash = bcrypt.generate_password_hash(
+                form.new_password.data.encode('utf8')
+            )
+            current_user.password_hash = password_hash.decode('utf8')
+            db.session.commit()
+            return render_template('index.html')
+        else:
+            flash('Check the current password and try again')
+            return render_template('edit_user_password.html', form=form)    
+    else:
+
+        return render_template('edit_user_password.html', form=form)
+
+
+@app.route('/delete_missing_person/<int:id>')
+@login_required
+def delete_missing_person(id):
+    missing_person = MissingPeople.query.filter_by(id=id).first()
+    db.session.delete(missing_person)
+    db.session.commit()
+    return redirect(url_for('missing_people'))
+
+@app.route('/delete_user/')
+@login_required
+def delete_user():
+    db.session.delete(current_user)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
--- app/model/forms.py
+++ app/model/forms.py
@@ -1,7 +1,9 @@
+import email
+from unicodedata import name
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
-from wtforms import StringField, PasswordField, BooleanField, EmailField
-from wtforms.validators import DataRequired, Email
+from wtforms import StringField, PasswordField, BooleanField, EmailField, DateField, TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, InputRequired
 
 
 class LoginForm(FlaskForm):
@@ -13,7 +15,49 @@ class LoginForm(FlaskForm):
 class SignUpForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
     email = EmailField('email', validators=[DataRequired(), Email()])
-    password = PasswordField('password', validators=[DataRequired()])
+    password = PasswordField('password', validators=[
+        DataRequired(),
+        Length(min=6, max=16)
+    ])
+    photo = FileField('image', validators=[
+        FileAllowed(['jpg', 'png'], 'Images only!')
+    ])
+
+class MissingPersonForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+    birthday = DateField('birthday', validators=[DataRequired()])
+    birthplace = StringField('birthplace', validators=[DataRequired()])
+    place_of_disappearance  = StringField('birthplace', validators=[DataRequired()])
+    disappearance_details = TextAreaField('disappearance_details', validators=[DataRequired()])
+    photo = FileField('image', validators=[
+        FileAllowed(['jpg', 'png'], 'Images only!'),
+        FileRequired()
+    ])
+
+class EditUserForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+    email = EmailField('email', validators=[DataRequired(), Email()])
+    photo = FileField('image', validators=[
+        FileAllowed(['jpg', 'png'], 'Images only!')
+    ])
+
+class EditUserPassword(FlaskForm):
+    current_password = PasswordField('old_password', validators=[DataRequired()])
+    new_password = PasswordField('password', validators=[
+        Length(min=6, max=16),
+        InputRequired(),
+        EqualTo('new_password', message='Passwords must match'),
+    ])
+    repeat_password = PasswordField(
+        'repeat_password',
+    )
+
+class EditMissingPerson(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+    birthday = DateField('birthday', validators=[DataRequired()])
+    birthplace = StringField('birthplace', validators=[DataRequired()])
+    place_of_disappearance  = StringField('birthplace', validators=[DataRequired()])
+    disappearance_details = TextAreaField('disappearance_details', validators=[DataRequired()])
     photo = FileField('image', validators=[
         FileAllowed(['jpg', 'png'], 'Images only!')
     ])
